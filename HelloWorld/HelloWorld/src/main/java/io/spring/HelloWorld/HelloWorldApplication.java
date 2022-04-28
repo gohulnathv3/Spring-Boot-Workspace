73a9d889c00cba7fed5fc9f0cb01349a7ca0cd0f
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-04-28 22:46:00 +0530

    Update HelloWorldApplication.java
    - Added HelloWorld to the Spring boot web services.

--- HelloWorld/HelloWorld/src/main/java/io/spring/HelloWorld/HelloWorldApplication.java
+++ HelloWorld/HelloWorld/src/main/java/io/spring/HelloWorld/HelloWorldApplication.java
@@ -2,12 +2,21 @@ package io.spring.HelloWorld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+	@RequestMapping("/hello")
+	public String sayHello(@RequestParam(value = "myName", defaultValue = "World")String name){
+		return String.format("Hello %s", name);
 	}
 
 }
