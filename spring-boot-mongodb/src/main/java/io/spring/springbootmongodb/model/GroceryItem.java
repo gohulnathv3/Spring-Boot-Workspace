41671011cedb377915843bdb1512ed95f3482ed3
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-05-27 19:15:53 +0530

    Create model for this app
    - We can create GroceryItem class file to store and retrieve data

--- spring-boot-mongodb/src/main/java/io/spring/springbootmongodb/model/GroceryItem.java
+++ spring-boot-mongodb/src/main/java/io/spring/springbootmongodb/model/GroceryItem.java
@@ -1,14 +1,55 @@
 package io.spring.springbootmongodb.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 
-@
+@Document("groceryitems") // this annotation is used to set the collection name that will be used by the model.
+// if the collection doesn't exist, Mongodb will create it.
 public class GroceryItem {
-    @Id
+    @Id // The mongodb's document primary key_id annotation.
     private String id;
     private String name;
     private int quantity;
     private String category;
 
+    public GroceryItem(String id, String name, int quantity, String category) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.category = category;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
