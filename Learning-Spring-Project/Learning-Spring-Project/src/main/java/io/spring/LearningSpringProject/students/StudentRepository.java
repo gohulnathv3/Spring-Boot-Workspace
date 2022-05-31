a1696b94d51fa4b653c53bb93efbde8ee687b0fe
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-05-31 13:10:18 +0530

    Create StudentRepository.java

---
+++ Learning-Spring-Project/Learning-Spring-Project/src/main/java/io/spring/LearningSpringProject/students/StudentRepository.java
@@ -0,0 +1,7 @@
+package io.spring.LearningSpringProject.students;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface StudentRepository extends MongoRepository<Student, String> {
+
+}
