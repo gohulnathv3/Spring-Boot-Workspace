b21e1360d78328628895c52d87667dd088d739e0
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-06-05 22:46:37 +0530

    Create DepartmentRepository.java

---
+++ Project-3/department-service/department-service/src/main/java/io/spring/department/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package io.spring.department.repository;
+
+import io.spring.department.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    Department findDepartmentById(Long departmentId);
+}
