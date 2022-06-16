5f2b32494cf397379aa06441480e306bd765254e
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-06-16 11:51:56 +0530

    Kafka consumer example
    - Here, we've added apache kafka dependency.
    - Next, we've created listener, model, and config packages

---
+++ kafka-consumer-springboot/kafka-consumer-springboot/src/main/java/io/spring/kafkaconsumerspringboot/KafkaConsumerSpringbootApplication.java
@@ -0,0 +1,13 @@
+package io.spring.kafkaconsumerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerSpringbootApplication.class, args);
+	}
+
+}
---
+++ kafka-consumer-springboot/kafka-consumer-springboot/src/test/java/io/spring/kafkaconsumerspringboot/KafkaConsumerSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.kafkaconsumerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
