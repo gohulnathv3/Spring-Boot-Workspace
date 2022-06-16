2f24ceed2ddd89d59fc719bf1f670fe64e1fc1f6
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-06-16 14:26:09 +0530

    Update KafkaConsumer.java

--- kafka-consumer-springboot/kafka-consumer-springboot/src/main/java/io/spring/kafkaconsumerspringboot/listener/KafkaConsumer.java
+++ kafka-consumer-springboot/kafka-consumer-springboot/src/main/java/io/spring/kafkaconsumerspringboot/listener/KafkaConsumer.java
@@ -1,5 +1,7 @@
 package io.spring.kafkaconsumerspringboot.listener;
 
+import io.spring.kafkaconsumerspringboot.model.User;
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServer;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -8,5 +10,9 @@ public class KafkaConsumer {
     @KafkaListener(topics = "topicOne", groupId = "group_id")
     public void consume(String message){
         System.out.println("Consumed message: "+message);
+    }
+    @KafkaListener(topics = "TopicOneJson",groupId = "group_json", containerFactory = "userConcurrentKafkaListenerContainerFactory")
+    public void consumeJSON(User user){
+        System.out.println("Consumed JSON message: "+user);
     }
 }
