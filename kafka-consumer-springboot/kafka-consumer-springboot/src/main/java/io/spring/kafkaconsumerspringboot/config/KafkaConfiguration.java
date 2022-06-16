ff7234729f46507d595adca1bb4ecc93ce9bbd20
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-06-16 12:07:36 +0530

    Update KafkaConfiguration.java
    - Adding @EnableKafka to start the service

--- kafka-consumer-springboot/kafka-consumer-springboot/src/main/java/io/spring/kafkaconsumerspringboot/config/KafkaConfiguration.java
+++ kafka-consumer-springboot/kafka-consumer-springboot/src/main/java/io/spring/kafkaconsumerspringboot/config/KafkaConfiguration.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
@@ -11,6 +12,7 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
+@EnableKafka
 @Configuration
 public class KafkaConfiguration {
 
