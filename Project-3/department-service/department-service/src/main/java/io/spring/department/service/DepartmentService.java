162a61c93916ee28049782c3ce52f9a71644e0d3
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-06-21 22:45:08 +0530

    Update DepartmentService.java

--- Project-3/department-service/department-service/src/main/java/io/spring/department/service/DepartmentService.java
+++ Project-3/department-service/department-service/src/main/java/io/spring/department/service/DepartmentService.java
@@ -5,8 +5,6 @@ import io.spring.department.repository.DepartmentRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 @Slf4j
@@ -23,6 +21,7 @@ public class DepartmentService {
 
     public Department findDepartmentById(Long departmentId) {
         log.info("Inside findDepartmentById method of DepartmentService");
-        return departmentRepository.findDepartmentById(departmentId);
+        return departmentRepository.findByDepartmentId(departmentId);
     }
+
 }
