090d7ea2313c2fd073e3b33a7ef9d89d4bb7676a
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-06-22 13:27:50 +0530

    Wikimedia to the database project

---
+++ springboot-kafka-realtime-project/springboot-kafka-realtime-project/src/test/java/io/spring/springbootkafkarealtimeproject/SpringbootKafkaRealtimeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.springbootkafkarealtimeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaRealtimeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
