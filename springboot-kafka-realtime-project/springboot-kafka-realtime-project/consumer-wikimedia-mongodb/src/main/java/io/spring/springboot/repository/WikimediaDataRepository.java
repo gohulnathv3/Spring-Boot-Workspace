436747e9c200c1063eb98c918a9d42bb4dd0f3d6
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-06-27 14:52:14 +0530

    Update WikimediaDataRepository.java

--- springboot-kafka-realtime-project/springboot-kafka-realtime-project/consumer-wikimedia-mongodb/src/main/java/io/spring/springboot/repository/WikimediaDataRepository.java
+++ springboot-kafka-realtime-project/springboot-kafka-realtime-project/consumer-wikimedia-mongodb/src/main/java/io/spring/springboot/repository/WikimediaDataRepository.java
@@ -2,6 +2,8 @@ package io.spring.springboot.repository;
 
 import io.spring.springboot.WikimediaData;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WikimediaDataRepository extends MongoRepository<WikimediaData, String> {
 }
