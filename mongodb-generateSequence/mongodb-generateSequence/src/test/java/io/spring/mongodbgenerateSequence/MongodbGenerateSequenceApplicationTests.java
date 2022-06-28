066f5e641f01d5ee7b0bebd61a5bca4dc09d8eb4
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-06-28 23:12:44 +0530

    Create MongodbGenerateSequenceApplicationTests.java

---
+++ mongodb-generateSequence/mongodb-generateSequence/src/test/java/io/spring/mongodbgenerateSequence/MongodbGenerateSequenceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.mongodbgenerateSequence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbGenerateSequenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
