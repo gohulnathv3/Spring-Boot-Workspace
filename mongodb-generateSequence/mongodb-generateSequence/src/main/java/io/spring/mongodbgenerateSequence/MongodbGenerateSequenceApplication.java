9dfd4c00878a2086dd144d8025e7c178cad82931
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-06-28 23:11:55 +0530

    Create MongodbGenerateSequenceApplication.java

---
+++ mongodb-generateSequence/mongodb-generateSequence/src/main/java/io/spring/mongodbgenerateSequence/MongodbGenerateSequenceApplication.java
@@ -0,0 +1,13 @@
+package io.spring.mongodbgenerateSequence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbGenerateSequenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbGenerateSequenceApplication.class, args);
+	}
+
+}
