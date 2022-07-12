be3b3c6018ae885cfeb5a903fce1ecec63c72b03
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-07-13 00:00:39 +0530

    Update DepartmentRepository.java

--- SPRING-PROJECT/department-service/department-service/src/main/java/com/springdemo/departmentservice/Repository/DepartmentRepository.java
+++ SPRING-PROJECT/department-service/department-service/src/main/java/com/springdemo/departmentservice/Repository/DepartmentRepository.java
@@ -4,4 +4,5 @@ import com.springdemo.departmentservice.Entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    Department findByDepartmentId(Long departmentId);
 }
