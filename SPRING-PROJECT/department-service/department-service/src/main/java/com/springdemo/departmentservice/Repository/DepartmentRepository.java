59688240dc6ea412394e3c820743657f2c4417ca
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-07-12 23:47:10 +0530

    Repository class added

---
+++ SPRING-PROJECT/department-service/department-service/src/main/java/com/springdemo/departmentservice/Repository/DepartmentRepository.java
@@ -0,0 +1,7 @@
+package com.springdemo.departmentservice.Repository;
+
+import com.springdemo.departmentservice.Entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
