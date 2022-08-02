52f3cd600ab1b12fd61022b4c76a79bfc3faaf21
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-08-02 13:20:05 +0530

    Added bean method

--- restful-webservices/restful-webservices/src/main/java/com/gohul/restfulwebservices/controller/HelloController.java
+++ restful-webservices/restful-webservices/src/main/java/com/gohul/restfulwebservices/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.gohul.restfulwebservices.controller;
 
+import com.gohul.restfulwebservices.service.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,5 +16,11 @@ public class HelloController {
 //    @RequestMapping(value = "/hello-world", method = RequestMethod.GET)
     public String helloWorld(){
         return "Hello World";
+    }
+
+    @GetMapping(value = "/hello-world-bean")
+//    @RequestMapping(value = "/hello-world", method = RequestMethod.GET)
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
     }
 }
