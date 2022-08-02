fe6d29642450c1523e8ea2bc9afbb4ca351d1cfc
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-08-02 13:03:50 +0530

    Unwrapping the project

---
+++ restful-webservices/restful-webservices/src/test/java/com/gohul/restfulwebservices/RestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gohul.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
