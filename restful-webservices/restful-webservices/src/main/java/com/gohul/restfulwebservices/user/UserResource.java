1cee892453ed0e53ed9d4b5acafcc6835caec8a8
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-08-02 14:46:16 +0530

    Controller class added

---
+++ restful-webservices/restful-webservices/src/main/java/com/gohul/restfulwebservices/user/UserResource.java
@@ -0,0 +1,38 @@
+package com.gohul.restfulwebservices.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    @Autowired
+    private UserDaoService service;
+
+    // retrieve all users "/users
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return service.findAll();
+    }
+    // retrieve user based on their id (int id)
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id){
+        return service.findOne(id);
+    }
+
+    // input - details of user
+    // output - Created & Return the created URI
+    // to create a user
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user){
+        User savedUser = service.save(user);
+    }
+
+
+
+
+
+}
