c2e846ab6f4dca5686a4098bfe5e29fbf3ba5435
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-08-03 21:54:03 +0530

    Set messages in @Size annotations

--- restful-webservices/restful-webservices/src/main/java/com/gohul/restfulwebservices/user/User.java
+++ restful-webservices/restful-webservices/src/main/java/com/gohul/restfulwebservices/user/User.java
@@ -5,7 +5,7 @@ import java.util.Date;
 
 public class User {
     private Integer id;
-    @Size(min = 2)
+    @Size(min = 2, message = "Name should atleast be having minimum of 2 characters")
     private String name;
     @Past
     private Date birthDate;
