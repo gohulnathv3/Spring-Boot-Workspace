dcffb5ba5375fe103ff858c214e301d8a7de2808
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-08-05 00:20:55 +0530

    Adding LocaleContextHolder

--- restful-webservices/restful-webservices/src/main/java/com/gohul/restfulwebservices/HelloWorld/HelloController.java
+++ restful-webservices/restful-webservices/src/main/java/com/gohul/restfulwebservices/HelloWorld/HelloController.java
@@ -3,6 +3,7 @@ package com.gohul.restfulwebservices.HelloWorld;
 import com.gohul.restfulwebservices.HelloWorld.HelloWorldBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -39,7 +40,8 @@ public class HelloController {
 
     @GetMapping("/hello-world-internationalized")
     public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale){
-        return messageSource.getMessage("good.morning.message",null,"Default Message",locale);
+        return messageSource.getMessage("good.morning.message",null,"Default Message",
+                LocaleContextHolder.getLocale());
         // en = Hello World
         // nl = Goede Morgen
         // fr = Bonjour
