3b813d681ceaf81c36420f69b67275ca0826a032
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-08-06 23:14:31 +0530

    Versioning RESTful services - Basic approach with URIs 1, Adding required classes and its constructors, getters and setters.

---
+++ restful-webservices/restful-webservices/src/main/java/com/gohul/restfulwebservices/versioning/PersonV2.java
@@ -0,0 +1,20 @@
+package com.gohul.restfulwebservices.versioning;
+
+public class PersonV2 {
+    private Name name;
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+
+    public PersonV2() {
+    }
+
+    public PersonV2(Name name) {
+        this.name = name;
+    }
+}
