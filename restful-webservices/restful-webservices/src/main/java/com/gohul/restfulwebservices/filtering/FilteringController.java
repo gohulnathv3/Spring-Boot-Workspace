9ab71b922cef55d44ece4a41f90fdffc7c3e8887
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-08-06 23:06:45 +0530

    Implementation of Dynamic filtering part 2

--- restful-webservices/restful-webservices/src/main/java/com/gohul/restfulwebservices/filtering/FilteringController.java
+++ restful-webservices/restful-webservices/src/main/java/com/gohul/restfulwebservices/filtering/FilteringController.java
@@ -25,7 +25,13 @@ public class FilteringController {
     }
     // to filter field1 and field3 for this get mapping
     @GetMapping("/filtering-list")
-    public List<SomeBean> retrieveListOfSomeBeans(){
-        return Arrays.asList(new SomeBean("value1","value2", "value3"), new SomeBean("value11", "value12", "value13"));
+    public MappingJacksonValue retrieveListOfSomeBeans() {
+        List<SomeBean> list = Arrays.asList(new SomeBean("value1", "value2", "value3"), new SomeBean("value11", "value12", "value13"));
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("field1", "field3");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+        MappingJacksonValue mapping = new MappingJacksonValue(list);
+        mapping.setFilters(filters);
+        return mapping;
     }
 }
