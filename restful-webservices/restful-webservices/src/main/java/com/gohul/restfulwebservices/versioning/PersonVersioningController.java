197f70a33ecfa0a3734a132472ac5bae92742897
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-08-06 23:30:33 +0530

    Header and Content Negotiation approach - I

--- restful-webservices/restful-webservices/src/main/java/com/gohul/restfulwebservices/versioning/PersonVersioningController.java
+++ restful-webservices/restful-webservices/src/main/java/com/gohul/restfulwebservices/versioning/PersonVersioningController.java
@@ -13,4 +13,13 @@ public class PersonVersioningController {
     public PersonV2 personV2(){
         return new PersonV2(new Name("Gohul","Nath"));
     }
+    @GetMapping(value = "/person/param", params = "version=1")
+    public PersonV1 paramV1(){
+        return new PersonV1("Gohul Nath");
+    }
+    @GetMapping(value = "/person/param", params = "version=2")
+    public PersonV2 paramV2(){
+        return new PersonV2(new Name("Gohul","Nath"));
+    }
+
 }
