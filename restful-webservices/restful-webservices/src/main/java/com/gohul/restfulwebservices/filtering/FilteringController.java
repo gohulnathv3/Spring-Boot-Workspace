3d1d17753932dd7647953442d64368735013a2cb
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-08-06 22:57:28 +0530

    Implementation of Dynamic filtering part 1

--- restful-webservices/restful-webservices/src/main/java/com/gohul/restfulwebservices/filtering/FilteringController.java
+++ restful-webservices/restful-webservices/src/main/java/com/gohul/restfulwebservices/filtering/FilteringController.java
@@ -1,5 +1,9 @@
 package com.gohul.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +12,18 @@ import java.util.List;
 
 @RestController
 public class FilteringController {
+    // to filter field1 and field2 for this get mapping
     @GetMapping("/filtering")
-    public SomeBean retrieveSomeBean(){
-        return new SomeBean("value1", "value2", "value3");
+    public MappingJacksonValue retrieveSomeBean(){
+        SomeBean someBean = new SomeBean("value1", "value2", "value3");
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("field1", "field2");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+        MappingJacksonValue mapping = new MappingJacksonValue(someBean);
+        mapping.setFilters(filters);
+        return mapping;
     }
+    // to filter field1 and field3 for this get mapping
     @GetMapping("/filtering-list")
     public List<SomeBean> retrieveListOfSomeBeans(){
         return Arrays.asList(new SomeBean("value1","value2", "value3"), new SomeBean("value11", "value12", "value13"));
